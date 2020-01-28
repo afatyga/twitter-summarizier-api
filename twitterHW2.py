c58c6e1a5ae3f95ae8901b9aa18a75625ab36085
Alex <afatyga@bu.edu>
2020-01-28 17:59:39 -0500

    start the twitter part - can get tweets and the link of the image

--- twitterHW2.py
+++ twitterHW2.py
@@ -1,16 +1,56 @@
+#EC500 HW 2
+#Alex Fatyga
+
 import keys
 import tweepy
+
+import io
+import os
+
+# Imports the Google Cloud client library
+from google.cloud import vision
+from google.cloud.vision import types
+
 def getMsgs(username):
 	auth = tweepy.OAuthHandler(keys.key, keys.secretKey)
 	auth.set_access_token(keys.accessToken, keys.accessTokenSecret)
 
 	api = tweepy.API(auth)
-	for tweet in tweepy.Cursor(api.search, q='tweepy').items(10):
-		print(tweet.text)
-
-#	public_tweets = api.home_timeline()
-#	for tweet in public_tweets:
+#	for tweet in tweepy.Cursor(api.search, q='tweepy').items(10):
 #		print(tweet.text)
+#		print(tweet.created_at)
+	for status in tweepy.Cursor(api.user_timeline,username).items(20):
+    # process status here
+		print(status.text)
+		try:
+			print(status.entities['media'][0]['media_url'])
+		except (NameError, KeyError):             #we dont want to have any entries without the media_url so lets do nothing
+			pass
+		else: #where we would do vision ai and all that fun stuff
+			print("\nah")
+				#if an image
+			#Instantiates a client
+#			client = vision.ImageAnnotatorClient()
+
+			#The name of the image file to annotate
+#			file_name = (status.entities['media'][0]['media_url'])
+
+#			Loads the image into memory
+#			with io.open(file_name, 'rb') as image_file:
+#			   content = image_file.read()
+
+#			image = types.Image(content=content)
+
+			# Performs label detection on the image file
+#			response = client.label_detection(image=image)
+#			labels = response.label_annotations
+
+#			print('Labels:')
+#			for label in labels:
+#			    print(label.description)
 
 
-getMsgs("afatyga")
+
+
+
+getMsgs("alexfatyga_")
