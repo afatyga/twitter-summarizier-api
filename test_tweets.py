22484710f592e614f550ea9c283ff5a8894ccd94
Alex <afatyga@bu.edu>
2020-02-04 16:39:56 -0500

    error checking

--- test_tweets.py
+++ test_tweets.py
@@ -1,4 +1,6 @@
 import twitterHW2
 
 def test_compare():
-	assert twitterHW2.getMsgs("alexfatyga_") == twitterHW2.getMsgs("alexfatyga_") 
+#	assert twitterHW2.getMsgs("alexfatyga_") == twitterHW2.getMsgs("alexfatyga_") 
+	assert twitterHW2.getMsgs(0) == "0"
+	assert twitterHW2.getMsgs("fakefakefakeusername1234211") == "0"
