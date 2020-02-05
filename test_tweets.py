d7f63c32301a97e486e0af6a59c35a6dab777314
afatyga <afatyga@bu.edu>
2020-02-05 17:34:31 -0500

    Update test_tweets.py

--- test_tweets.py
+++ test_tweets.py
@@ -5,4 +5,4 @@ def test_compare():
 #	assert twitterHW2.getMsgs("alexfatyga_") == twitterHW2.getMsgs("alexfatyga_") 
 #	assert twitterHW2.getMsgs("alexfatyga_") == 1 #1 denotes success
 	assert twitterHW2.getMsgs(123) == 0 #0 denotes fail
-#	assert twitterHW2.getMsgs("fakefakefakeusername1234211") == "0"
+#	assert twitterHW2.getMsgs("fakefakefakeusername1234211") == 0
